e15fc6fcd6b13cb68e74a03af5bed7b358a151e5
Yixiao Li, Macbook Air <yxli2123@gmail.com>
2023-06-28 13:23:01 -0400

    d

--- run_glue.py
+++ run_glue.py
@@ -495,8 +495,8 @@ def main():
     )
 
     # Prepare everything with our `accelerator`.
-    model, optimizer, train_dataloader, eval_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, eval_dataloader, lr_scheduler
+    model, model_teacher, optimizer, train_dataloader, eval_dataloader, lr_scheduler = accelerator.prepare(
+        model, model_teacher, optimizer, train_dataloader, eval_dataloader, lr_scheduler
     )
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed
